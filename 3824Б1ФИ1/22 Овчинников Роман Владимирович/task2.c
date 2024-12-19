3ce80b7ae8db100b2ea2504f59d0d7f80e6095f0
Polomb1 <20060711roma@mail.ru>
2024-12-19 09:56:57 +0300

    Update task2.c

--- 3824Б1ФИ1/22 Овчинников Роман Владимирович/task2.c
+++ 3824Б1ФИ1/22 Овчинников Роман Владимирович/task2.c
@@ -1,3 +1,4 @@
+#include <float.h>
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Найти индексы k1 и k2 двух элементов массива, 
 // расстояние |A[k1] – A[k2]| между которыми минимально.
@@ -5,11 +6,45 @@
 // При невозможности найти пару, вернуть {-1,-1}.
 struct Indices
 {
-  int first_index;
-  int second_index;
+	int first_index;
+	int second_index;
 };
 struct Indices task2(double A[], size_t n)
 {
-  struct Indices indices = {-2, -2};
-  return indices;
+	struct Indices indices = { -1, -1 };
+	if (n > 1)
+	{
+		double m = 0.0, minim = DBL_MAX;
+		for (int a = 0; a < n; a++)
+		{
+			if (a + 1 < n)
+			{
+				for (int b = a+1; b < n; b++)
+				{
+					m = A[a] - A[b];
+					if (m < 0)
+						m = -m;
+					if (minim > m)
+						minim = m;
+				}
+			}
+		}
+		for (int a = 0; a < n; a++)
+		{
+			if (a + 1 < n)
+			{
+				for (int b = a + 1; b < n; b++)
+				{
+					if (((A[a] - A[b]) == minim) || ((A[a] - A[b]) == -minim))
+					{
+						indices = { a, b };
+						break;
+					}
+				}
+			}
+		}
+		return indices;
+	}
+	else
+		return indices;
 }
