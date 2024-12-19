9ff1578a5bba25e6eeee162c15bb212a93a0c93f
Polomb1 <20060711roma@mail.ru>
2024-12-19 09:26:23 +0300

    Update task1.c

--- 3824Б1ФИ1/22 Овчинников Роман Владимирович/task1.c
+++ 3824Б1ФИ1/22 Овчинников Роман Владимирович/task1.c
@@ -3,5 +3,25 @@
 // иначе вернуть 0.
 int task1(unsigned A[], size_t n)
 {
-  return -1;
+	if (n > 1)
+	{
+		unsigned int a = A[1] - A[0], b = 0; char flag = 1;
+		for (int i = 0; i < n-1; i++)
+		{
+			b = A[i + 1] - A[i];
+			if (a != b)
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+			return 1;
+		else
+			return 0;
+	}
+	else
+	{
+		return 0;
+	}
 }
