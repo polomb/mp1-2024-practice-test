69e09186578882bf6f39e1de3b08cb747a402b13
Polomb1 <20060711roma@mail.ru>
2024-12-19 10:17:03 +0300

    Update task3.c

--- 3824Б1ФИ1/22 Овчинников Роман Владимирович/task3.c
+++ 3824Б1ФИ1/22 Овчинников Роман Владимирович/task3.c
@@ -1,5 +1,13 @@
+#include <string.h>
 // Задача 3. Дана строка s. Подсчитать количество содержащихся в ней цифр.
-size_t task3(char *s)
+size_t task3(char* s)
 {
-  return -1;
+	unsigned long long len = strlen(s);
+	unsigned long long k = 0;
+	for (unsigned long long i = 0; i < len; i++)
+	{
+		if ((s[i] >= '0' && s[i] <= '9'))
+			k++;
+	}
+	return k;
 }
